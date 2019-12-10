0efbf913f07d854aba134c5b06e47e1443326560
Erica McConnaughey <ericamcconnaughey@gmail.com>
2019-12-10 12:17:55 -0600

    add code for third spec

--- js/scripts.js
+++ js/scripts.js
@@ -1,29 +1,18 @@
 //Business Logic
 var nums = [];
-var str = '<ul>';
-
+//var str = '<ul>';
 
 var beepBoop = function(num) {
   for (let currentNum = 0; currentNum <= num; currentNum++) {
-    if (currentNum.toString().includes("1")) {
+    if (currentNum.toString().includes("2")) {
+      nums.push("Boop!");
+    } else if (currentNum.toString().includes("1")) {
       nums.push("Beep!");
     } else {
     nums.push(currentNum);
     }
   }
   return nums;
-
-  // if () {
-
-  // } else {
-    
-  // }
-
-  // if ((year % 100 !== 0) && (year % 4 === 0) || (year % 400 === 0)) {
-  //   return true;
-  // } else {
-  //   return false;
-  // }
 };
 
 
